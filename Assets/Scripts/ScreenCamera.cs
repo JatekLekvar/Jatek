2d3595ee3c2f8a97c11f6289ba7d188260e7295e
garnold02 <gulyas.arnold02@protonmail.com>
2023-02-15 12:43:10 +0100

    Camera improvements

--- Assets/Scripts/ScreenCamera.cs
+++ Assets/Scripts/ScreenCamera.cs
@@ -3,8 +3,36 @@ using UnityEngine;
 public class ScreenCamera : MonoBehaviour
 {
     public Transform target;
+
+    float limitX = 2f;
+    float limitY = 2f;
+
     void Update()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, -10f);
+        Vector3 diff = target.position - transform.position;
+
+        if (diff.x > limitX)
+        {
+            float disp = diff.x - limitX;
+            transform.Translate(new Vector3(disp, 0f, 0f));
+        }
+
+        if (diff.x < -limitX)
+        {
+            float disp = diff.x + limitX;
+            transform.Translate(new Vector3(disp, 0f, 0f));
+        }
+
+        if (diff.y > limitY)
+        {
+            float disp = diff.y - limitY;
+            transform.Translate(new Vector3(0f, disp, 0f));
+        }
+
+        if (diff.y < -limitY)
+        {
+            float disp = diff.y + limitY;
+            transform.Translate(new Vector3(0f, disp, 0f));
+        }
     }
 }
