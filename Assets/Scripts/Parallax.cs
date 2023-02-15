8ed55c5a4b9307a48cd05584043cbd1d2ab22e9d
garnold02 <gulyas.arnold02@protonmail.com>
2023-02-15 19:53:21 +0100

    Fix conflicts

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public List<Transform> foregroundLayers;
+    public List<Transform> backgroundLayers;
+    public Transform target;
+
+    void Update()
+    {
+
+    }
+}
