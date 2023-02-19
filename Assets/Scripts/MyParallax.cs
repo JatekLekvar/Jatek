4c25926cb4aa658ef349cbdaf4ed88ac8c947eaf
IncepT01 <94876813+IncepT01@users.noreply.github.com>
2023-02-19 08:25:01 +0100

    8 30 AM

--- Assets/Scripts/MyParallax.cs
+++ Assets/Scripts/MyParallax.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class MyParallax : MonoBehaviour
 {
-    public List<Transform> backgroundLayers;
-    public Transform foregroundLayer;
-    public Transform target;
-    public bool hasForegorund = true;
+    //public List<Transform> backgroundLayers;
+    //public Transform foregroundLayer;
+    //public Transform target;
+    //public bool hasForegorund = true;
 
     [SerializeField] private Vector2 parallaxEffectMultiplier;
 
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
+    private SceneSpecs sceneSpecs;
 
     private void Start(){
         cameraTransform = Camera.main.transform;
         lastCameraPosition = cameraTransform.position;
+        sceneSpecs = GameObject.Find("Scene Specs").GetComponent<SceneSpecs>();
     }
+
+
 
     private void LateUpdate(){
         Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y,0f);
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, (sceneSpecs.maxY + sceneSpecs.minY)/2,0f);
+        transform.position = new Vector3(transform.position.x, (sceneSpecs.maxY + sceneSpecs.minY)/2,transform.position.z);
         lastCameraPosition = cameraTransform.position;
     }
 }
